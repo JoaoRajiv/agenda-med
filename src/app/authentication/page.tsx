5088b68821fab50c2ab084ee437eee3e7300639a
JoaoRajiv <olivairarajiv@gmail.com>
2026-05-27 01:21:32 -0300

    feat: implement authentication page with login and sign-up forms

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,30 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/src/components/ui/tabs";
+
+import SignUpForm from "./components/sign-up-form";
+import LoginForm from "./components/login-form";
+
+const AuthenticationPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-100">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginForm />
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Login Page</h1>
-    </div>
-  );
-};
-
-export default LoginPage;
