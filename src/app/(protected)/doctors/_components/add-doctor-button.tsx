d341c378c85c5e94e4541bf89f3c90bcbc212a9c
JoaoRajiv <olivairarajiv@gmail.com>
2026-06-01 22:56:21 -0300

    feat: add doctor creation form

--- src/app/(protected)/doctors/_components/add-doctor-button.tsx
+++ src/app/(protected)/doctors/_components/add-doctor-button.tsx
@@ -10,50 +10,18 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Plus } from "lucide-react";
+import UpsertDoctorForm from "./upsert-doctor-form";
 
 const AddDoctorButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" size="sm">
-          <Plus className="mr-2" />
+        <Button size="sm">
+          <Plus />
           Adicionar Médico
         </Button>
       </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Adicionar Médico</DialogTitle>
-          <DialogDescription>
-            Preencha as informações do médico para adicioná-lo à sua clínica.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          {/* Formulário para adicionar médico */}
-          <div className="grid w-full items-center gap-2">
-            <label className="text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-              Nome do Médico
-            </label>
-            <input
-              type="text"
-              placeholder="Digite o nome do médico"
-              className="border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border bg-transparent px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-          <div className="grid w-full items-center gap-2">
-            <label className="text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-              Especialidade
-            </label>
-            <input
-              type="text"
-              placeholder="Digite a especialidade do médico"
-              className="border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border bg-transparent px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Salvar</Button>
-        </DialogFooter>
-      </DialogContent>
+      <UpsertDoctorForm />
     </Dialog>
   );
 };
